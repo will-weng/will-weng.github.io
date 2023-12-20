b871d2e589b880218da9251b321c3439294cd47b
will-weng <william.weng@hotmail.com>
2023-12-20 19:54:20 +1100

    Restarted project from scratch

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Page for William Weng, under development</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
