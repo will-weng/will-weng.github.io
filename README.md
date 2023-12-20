d1a3557dd872b1e834b546d372be70c8ffc10f82
will-weng <william.weng@hotmail.com>
2023-12-20 18:40:07 +1100

    Converted to TS, cleaned up files

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Personal webpage!
-[visit my page](https://will-weng.io/)
+[visit my page](https://will-weng.github.io/)
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <body>
+        <p>This is my page testing</p>
+      </body>
+    </div>
+  );
+}
+
+export default App;
