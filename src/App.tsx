203d2f3f8f584050830a68c2dbf3fbac919933ec
will-weng <william.weng@hotmail.com>
2023-12-20 18:53:24 +1100

    formated code, removed styles and moved body to header

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 import './styles/App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <body>
-        <p>This is my page testing</p>
-      </body>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header>
+                <h1>This is my page William Weng</h1>
+            </header>
+            <body>
+            </body>
+        </div>
+    );
 }
 
 export default App;
