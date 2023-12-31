dd8d851929b385d69a51193862fc0b0a4a839366
will-weng <william.weng@hotmail.com>
2023-12-31 18:16:53 +1100

    Added comment for palette

--- src/components/styles/Theme.ts
+++ src/components/styles/Theme.ts
@@ -1,6 +1,5 @@
-import { ThemeConsumer } from "styled-components";
-
 const theme = {
+    // https://coolors.co/palette/003049-d62828-f77f00-fcbf49-eae2b7
     colours: {
         PrussianBlue: '#003049',
         FireEngineRed: '#d62828ff',
