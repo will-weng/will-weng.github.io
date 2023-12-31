daa37275246ea49a44498f04d37b2f917f1c0e9b
will-weng <william.weng@hotmail.com>
2023-12-31 18:23:43 +1100

    Added email address

--- src/components/Address.tsx
+++ src/components/Address.tsx
@@ -1,6 +1,8 @@
 function Address() {
     return (<address>
         <a href="https://github.com/will-weng"> My github!</a>
+        <br />
+        <a href="mailto:william.weng@hotmail.com">william.weng@hotmail.com</a>
     </address>)
 }
 
