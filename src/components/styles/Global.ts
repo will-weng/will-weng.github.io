7d4bb1309c0ba0a25c0b8e4998a9ebbb6ec45276
will-weng <william.weng@hotmail.com>
2023-12-31 17:40:26 +1100

    css reset styles

--- src/components/styles/Global.ts
+++ src/components/styles/Global.ts
@@ -3,11 +3,12 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyles = createGlobalStyle`
     * {
         box-sizing: border-box;
-        color: ${({ theme }) => theme.colors.text}
+        color: ${({ theme }) => theme.colors.text};
+        margin: 0;
     }
-    
+
     body {
-        background-color: ${({ theme }) => theme.colors.body};
+        background-color: ${({ theme }) => theme.colors.body};  line-height: 1.5;
     }
 `
 
