8d9a98e6db7f30f7aa32f4602f51bd694c82d4e7
will-weng <william.weng@hotmail.com>
2024-12-04 02:27:44 +1100

    update about me section

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -7,7 +7,10 @@ function AboutMe() {
     return (
         <AboutMeStyle>
             <h1>About me</h1>
-            <p>Text about me!</p>
+            <p>Hello! I’m William, a curious problem-solver, tech enthusiast, and passionate developer. My interests span STEM and finance, and I enjoy exploring how technology can create impactful solutions.</p>
+            <p>Currently, I’m sharpening my skills as a full-time software developer, committed to continuous learning and staying adaptive in an ever-evolving tech landscape.</p>
+            <p>Outside work, I have a variety of hobbies that keep life exciting. Lately, I’ve been captivated by poker, drawn to its fascinating blend of strategy, probability, and psychology - a balance that mirrors my love for programming.</p>
+            <p>Looking ahead, I’m eager to collaborate on projects that tackle meaningful challenges, combine creativity with technology, and make a difference. Whether you want to discuss tech, share ideas, or trade poker tips, let’s connect!</p>
         </AboutMeStyle>
     );
 }
